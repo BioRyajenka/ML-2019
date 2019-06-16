5221468e880e43f668e49d8c5aef321215f37ebd
oxygen311 <alex39-09@mail.ru>
2019-06-16 19:35:13 +0300

    100 traders data

---
+++ model_evaluator.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+class Evaluator():
+    def __init__(self, file, start, end):
+        df = pd.read_csv(file)
+
+        starts = np.array(df.loc[df['TIME'] == start].values[0][1:])
+        ends = np.array(df.loc[df['TIME'] == end].values[0][1:])
+
+        self.returns = {}
+        for trader_id, rt in zip(df.columns.values[1:], ends - starts):
+            self.returns[trader_id] = rt / 100
+
+    def evaluate_portfolio(self, portfolio):
+        def normilize_portfolio():
+            summ = np.sum(list(portfolio.values()))
+            for key in portfolio.keys():
+                portfolio[key] /= summ
+
+        normilize_portfolio()
+        res = 0
+        for key, value in portfolio.items():
+            res += value * (self.returns[key])
+        return res * 100
